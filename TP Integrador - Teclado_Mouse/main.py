31262fb6b823a583258394ea4214aa1dcced70e8
BrunoColantonio <colantoniobruno166@gmail.com>
2025-07-09 01:57:07 -0300

    candidate version

--- TP Integrador - Teclado_Mouse/main.py
+++ TP Integrador - Teclado_Mouse/main.py
@@ -5,51 +5,51 @@ import math
 from pynput.keyboard import Controller, Key
 from time import sleep
 import ctypes
+import collections
+import time
 
 import mouse
-
-
 
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
 
-#Comment when using mouse
-# frameWidth = 1280
-# frameHeight = 720
-
-#Uncomment to use mouse
 frameWidth = 1920
 frameHeight = 1080
 
 frame_reduction = 100
-smoothening = 7
-
+smoothening = 12
 
 
 SHOW_KEYBOARD_BTN_X = 650
 SHOW_KEYBOARD_BTN_Y = 5
-PAD_X = 700
-PAD_Y = 450
-PAD_WIDTH = frameWidth-700
-PAD_HEIGHT = frameHeight-450
 
-pTime = 0
 PLOCX = 0
 PLOCY = 0
-
 
 cam = cv2.VideoCapture(0)
 cam.set(3, frameWidth)
 cam.set(4, frameHeight)
 
-
-# screenWidth, screenHeight = autopy.screen.size()
-
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","<-"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";","->"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/","(X)"]]
 finalText = ""
+
+# Global variable to show keyboard
+show_keyboard = True
+
+# Global variables to control frame ignoration
+ignore_frame = False
+frames_to_ignore = 5
+
+# Globals to control tab switch
+SWIPE_THRESHOLD = 400  # pixels
+COOLDOWN_TIME = 1.0  # seconds
+last_swipe_time = 0 
+
+# Global wrist history arr
+wrist_history = collections.deque(maxlen=10)
 
 keyboard = Controller()
 
@@ -117,43 +117,70 @@ def drawNoKeyboard(img):
     out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
     return out
 
-def drawPad(img):
-    imgNew = np.zeros_like(img, np.uint8)
-    x = 0
-    y = frameHeight
-    cornerRect(imgNew, (PAD_X, PAD_Y, PAD_WIDTH, PAD_HEIGHT),20, rt=0)
-    cv2.rectangle(imgNew, (PAD_X,PAD_Y), (PAD_X + 1500, PAD_Y + 1500),(200, 200, 200), cv2.FILLED)
-
-
-    
-    out = img.copy()
-    alpha = 0.5
-    mask = imgNew.astype(bool)
-    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
-    return out
-
 def checkMouse(index_x_pos,index_y_pos,middle_x_pos,middle_y_pos,plocX,plocY):
         
         x3 = np.interp(middle_x_pos, (100, 640 - 100), (0, screensize[0]))
         y3 = np.interp(middle_y_pos, (100, 480 - 100), (0, screensize[1]))
-        # 6. Smoothen Values
+        # Smoothen Values
         clocX = plocX + (x3 - plocX) / smoothening
         clocY = plocY + (y3 - plocY) / smoothening
     
-        # 7. Move Mouse
-        # autopy.mouse.move(wScr - clocX, clocY)
+        # Move Mouse
         mouse.move(screensize[0]-clocX,clocY)
 
         plocX = clocX
         plocY = clocY
 
         distance = math.hypot((index_x_pos-middle_x_pos), (index_y_pos-middle_y_pos))
-        if(distance < 40):
+        if(distance < 60):
              mouse.click("left")
-
 
         return plocX,plocY
 
+def checkMultiDesktop(index_tip_x_pos,index_tip_y_pos,thumb_tip_x_pos,thumb_tip_y_pos):
+     global show_keyboard
+     global ignore_frame
+     distanceIndexThumb = math.hypot((index_tip_x_pos-thumb_tip_x_pos), (index_tip_y_pos-thumb_tip_y_pos))
+     if(distanceIndexThumb < 150):
+        with keyboard.pressed(keyboard._Key.cmd):  # Key.cmd is the Windows key on Windows
+             keyboard.press(keyboard._Key.tab)
+             keyboard.release(keyboard._Key.tab)
+        
+        show_keyboard = False
+        ignore_frame = True
+        sleep(0.50)
+        return True
+     else:
+         return False
+
+def checkSwapTabs(wrist_history):
+    global show_keyboard
+    global frames_to_ignore
+    global ignore_frame
+    global last_swipe_time
+
+    if len(wrist_history) >= 5:
+        movement = wrist_history[-1] - wrist_history[0]
+
+        if abs(movement) > SWIPE_THRESHOLD and (time.time() - last_swipe_time) > COOLDOWN_TIME:
+            wrist_history.clear()
+            with keyboard.pressed(keyboard._Key.alt):
+                keyboard.press(keyboard._Key.tab)
+                keyboard.release(keyboard._Key.tab)
+            
+            
+
+            show_keyboard = False
+            ignore_frame = True
+            sleep(0.50)
+            last_swipe_time = time.time()
+     
+def isOverKeyboardBtn(middle_tip_x_pos,middle_tip_y_pos):
+    if(SHOW_KEYBOARD_BTN_X < middle_tip_x_pos  and middle_tip_x_pos < SHOW_KEYBOARD_BTN_X+65
+                   and SHOW_KEYBOARD_BTN_Y < middle_tip_y_pos  and middle_tip_y_pos < SHOW_KEYBOARD_BTN_Y+65):
+         return True
+    else:
+        return False
 
 class Key():
     def __init__(self, pos, text, size=[65, 65]):
@@ -178,11 +205,15 @@ hands = mp_hands.Hands(
      min_detection_confidence=0.5)
 
 
+
 show_keyboard = True
 while True:
+
     #Obtengo imagen de webcam
     success, frame = cam.read()
-    frame = cv2.flip(frame,1)
+
+    if show_keyboard:
+        frame = cv2.flip(frame,1)
 
     #Draw interface
     if(show_keyboard == True):
@@ -190,82 +221,112 @@ while True:
     else:
         frame = drawNoKeyboard(frame)
 
-    # frame = drawPad(frame)
-
     frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
 
     #Get finger tracking
     results = hands.process(frame_rgb)
-
+    num_hands = 1
     if results.multi_hand_landmarks is not None:
+        num_hands = len(results.multi_hand_landmarks)
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame,hand_landmarks,mp_hands.HAND_CONNECTIONS)
-            # index_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * 1280
-            # index_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * 720
-            # middle_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * 1280
-            # middle_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * 720
 
-            index_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * frameWidth
-            index_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * frameHeight
-            middle_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * frameWidth
-            middle_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * frameHeight
+            #Get index tip
+            index_tip_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * frameWidth
+            index_tip_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * frameHeight
 
+            #Get middle tip
+            middle_tip_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * frameWidth
+            middle_tip_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * frameHeight
+
+            # Get middle tip
+            thumb_tip_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * frameWidth
+            thumb_tip_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * frameHeight
+
+            # Get middle mcp
+            middle_mcp_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x * frameWidth
+            middle_mcp_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y * frameHeight
+
+            # Get wrist
+            wrist_x_pos = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x * frameWidth
+            wrist_y_pos = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y * frameHeight
+
+            # Ignore frame when flipping camera
+            if  ignore_frame:
+                ignore_frame = False
+                sleep(0.50)
+                break
+
+            if num_hands == 1:
+                wrist_history.append(wrist_x_pos)
+
+                did_gesture = checkMultiDesktop(index_tip_x_pos,index_tip_y_pos,thumb_tip_x_pos,thumb_tip_y_pos)
+                if did_gesture:
+                    wrist_history.clear()
+                    did_gesture = not did_gesture
+                
+                checkSwapTabs(wrist_history)
 
 
             if(show_keyboard == False):
-                PLOCX,PLOCY = checkMouse(index_x_pos,index_y_pos,middle_x_pos,middle_y_pos,PLOCX,PLOCY)
-                if(SHOW_KEYBOARD_BTN_X < middle_x_pos  and middle_x_pos < SHOW_KEYBOARD_BTN_X+65
-                   and SHOW_KEYBOARD_BTN_Y < middle_y_pos  and middle_y_pos < SHOW_KEYBOARD_BTN_Y+65):
-                            cv2.rectangle(frame, (SHOW_KEYBOARD_BTN_X - SHOW_KEYBOARD_BTN_Y, SHOW_KEYBOARD_BTN_Y - 5), (SHOW_KEYBOARD_BTN_X + 65 + 5, SHOW_KEYBOARD_BTN_Y + 65 + 5), (30, 144, 255), cv2.FILLED)
-                            cv2.putText(frame, "(X)", (SHOW_KEYBOARD_BTN_X, SHOW_KEYBOARD_BTN_Y+40),cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
-                            show_keyboard = not show_keyboard
-                            cv2.rectangle(frame, key.pos, (SHOW_KEYBOARD_BTN_X + 65, SHOW_KEYBOARD_BTN_Y + 65), (30, 144, 255), cv2.FILLED)
-                            cv2.putText(frame, key.text, (SHOW_KEYBOARD_BTN_X + 20, SHOW_KEYBOARD_BTN_Y + 65),
-                                        cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
-                            sleep(0.50)
+                PLOCX,PLOCY = checkMouse(index_tip_x_pos,index_tip_y_pos,middle_tip_x_pos,middle_tip_y_pos,PLOCX,PLOCY)
+                if(isOverKeyboardBtn(middle_tip_x_pos,middle_tip_y_pos)):
+                    cv2.rectangle(frame, (SHOW_KEYBOARD_BTN_X - SHOW_KEYBOARD_BTN_Y, SHOW_KEYBOARD_BTN_Y - 5), (SHOW_KEYBOARD_BTN_X + 65 + 5, SHOW_KEYBOARD_BTN_Y + 65 + 5), (30, 144, 255), cv2.FILLED)
+                    cv2.putText(frame, "(X)", (SHOW_KEYBOARD_BTN_X, SHOW_KEYBOARD_BTN_Y+40),cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
+                    
+                    show_keyboard = True
+                    ignore_frame = True
+                    wrist_history.clear()
+
+                    # cv2.rectangle(frame, key.pos, (SHOW_KEYBOARD_BTN_X + 65, SHOW_KEYBOARD_BTN_Y + 65), (30, 144, 255), cv2.FILLED)
+                    # cv2.putText(frame, key.text, (SHOW_KEYBOARD_BTN_X + 20, SHOW_KEYBOARD_BTN_Y + 65),
+                    #             cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+                    sleep(0.50)
             else:
                 for key in keyList:
                     x,y = key.pos
                     w,h = key.size
 
 
-                    distance = math.hypot((middle_x_pos-index_x_pos), (middle_y_pos-index_y_pos))
-                    if(x < middle_x_pos  and middle_x_pos < x+w
-                    and y < middle_y_pos  and middle_y_pos < y+h):
+                    distance = math.hypot((middle_tip_x_pos-index_tip_x_pos), (middle_tip_y_pos-index_tip_y_pos))
+                    if(x < middle_tip_x_pos  and middle_tip_x_pos < x+w
+                    and y < middle_tip_y_pos  and middle_tip_y_pos < y+h):
                             cv2.rectangle(frame, (x - 5, y - 5), (x + w + 5, y + h + 5), (30, 144, 255), cv2.FILLED)
                             if(key.text in ("<-","->","(X)")):
                                 cv2.putText(frame, key.text, (x, y+40),cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
                             else:
                                 cv2.putText(frame, key.text, (x + 15, y + 50),cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
                             
-                            if(distance < 40):
+                            if(distance < 60):
                                 if(key.text == "<-"):
                                     keyboard.press(keyboard._Key.backspace)
                                     finalText = finalText[:-1]
+
                                 elif(key.text == "->"):
                                     keyboard.press(keyboard._Key.enter)
                                     finalText = ""
+
                                 elif(key.text == "(X)"):
-
-                                    #Uncomment to use mouse
                                     show_keyboard = not show_keyboard
+                                    ignore_frame = True
+                                    wrist_history.clear()
 
-                                    #Comment when use mouse
-                                    # cam.release()
-                                    # cv2.destroyAllWindows()
                                 else:
                                     keyboard.press(key.text)
                                     finalText += key.text
+
                                 cv2.rectangle(frame, key.pos, (x + w, y + h), (30, 144, 255), cv2.FILLED)
-                                cv2.putText(frame, key.text, (x + 20, y + 65),
-                                            cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+                                cv2.putText(frame, key.text, (x + 20, y + 65),cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
                                 sleep(0.50)
 
                 #Show written text
                 cv2.rectangle(frame, (50, 350), (700, 450), (30, 144, 255), cv2.FILLED)
                 cv2.putText(frame, finalText, (60, 430),
                             cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 5)
+                
+    else:
+        wrist_history.clear()
 
     cv2.imshow("Resultado",frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
